25fe7491f9b32b138e63736d3234741bd00a745a
Londeka <Samkelospringt@gmail.com>
2022-06-29 12:36:34 +0200

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,10 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'myapp';
+  loadedFeature = 'recipe'
+
+
+  onNavigate(feature: any){
+  this.loadedFeature= feature;
+  }
 }
